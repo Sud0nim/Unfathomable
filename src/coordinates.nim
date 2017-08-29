cc3766111309a5c3f3858a377f421409d62039ed
Sud0nim <Sud0nim@users.noreply.github.com>
2017-08-29 20:42:28 +1000

    Update coordinates.nim

--- src/coordinates.nim
+++ src/coordinates.nim
@@ -37,87 +37,50 @@ proc getBearing*(pointA, pointB: Point): float =
  
 #[
 WIP: Parsing string coordinates
-proc parseCoordinates(a: string): float =
+proc parseCoordinates(latitudeOrLongitude: string): float =
   var 
-    b = a.split
-    latitude = 0.0
-    longitude = 0.0
-  if b.len == 8:
+    b = latitudeOrLongitude.split
+    parsedCoordinate: float
+  if b.len == 4:
     try:
       var
-        latDegrees = parseFloat(b[0].replace("°", ""))
-        latMinutes = parseFloat(b[1].replace("′", ""))
-        latSeconds = parseFloat(b[2].replace("″", ""))
-        latBearing = b[3] #make proc to parseDirection and return an enum of North, South, East West
-        latitude = latDegrees + latMinutes / 60 + latSeconds / 3600
-      if latBearing in ["s", "S"]: #improve this with parseDirection proc
-        latitude *= -1.0
+        degrees = parseFloat(b[0].replace("°", ""))
+        minutes = parseFloat(b[1].replace("′", ""))
+        seconds = parseFloat(b[2].replace("″", ""))
+        bearing = b[3] #make proc to parseDirection and return an enum of North, South, East West
+        parsedCoordinate = degrees + minutes / 60 + seconds / 3600
+      if bearing in ["s", "S"]: #improve this with parseDirection proc
+        parsedCoordinate *= -1.0
     except IOError:
-        ## handle errors for latitude
+        ## handle errors  
+  elif b.len == 3:
     try:
       var
-        longDegrees = parseFloat(b[4].replace("°", ""))
-        longMinutes = parseFloat(b[5].replace("′", ""))
-        longSeconds = parseFloat(b[6].replace("″", ""))
-        longBearing = b[7]
-        longitude = longDegrees + longMinutes / 60 + longSeconds / 3600
-      if longBearing in ["w", "W"]: #improve this with parseDirection
-        longitude *= -1.0
+        degrees = parseFloat(b[0].replace("°", ""))
+        minutes = parseFloat(b[1].replace("′", ""))
+        bearing = b[2] #make proc to parseDirection and return an enum of North, South, East West
+        parsedCoordinate = degrees + minutes / 60
+      if bearing in ["s", "S"]: #improve this with parseDirection proc
+        parsedCoordinate *= -1.0
     except IOError:
-        ## handle errors for longitude
-  elif b.len == 6:
-    try:
-      var
-        latDegrees = parseFloat(b[0].replace("°", ""))
-        latMinutes = parseFloat(b[1].replace("′", ""))
-        latBearing = b[2]
-        latitude = latDegrees + latMinutes / 60
-      if latBearing in ["s", "S"]: #improve this with parseDirection
-        latitude *= -1.0
-    except IOError:
-        ## handle errors for latitude
-    try:
-      var
-        longDegrees = parseFloat(b[3].replace("°", ""))
-        longMinutes = parseFloat(b[4].replace("′", ""))
-        longBearing = b[5]
-        longitude = longDegrees + longMinutes / 60
-      if longBearing in ["w", "W"]: #improve this with parseDirection
-        longitude *= -1.0
-    except IOError:
-        ## handle errors for longitude
-  elif b.len == 4:
-    try:
-      var
-        latDegrees = parseFloat(b[0].replace("°", ""))
-        latBearing = b[2]
-        latitude = latDegrees
-      if latBearing in ["s", "S"]: #improve this with parseDirection
-        latitude *= -1.0
-    except IOError:
-        ## handle errors for latitude
-    try:
-      var
-        longDegrees = parseFloat(b[3].replace("°", ""))
-        longBearing = b[5]
-        longitude = longDegrees
-      if longBearing in ["w", "W"]: #improve this with parseDirection
-        longitude *= -1.0
-    except IOError:
-        ## handle errors for longitude
+        ## handle errors
   elif b.len == 2:
     try:
-      latitude = parseFloat(b[0])
-      longitude = parseFloat(b[1])
+      var
+        degrees = parseFloat(b[0].replace("°", ""))
+        bearing = b[1] #make proc to parseDirection and return an enum of North, South, East West
+        parsedCoordinate = degrees
+      if bearing in ["s", "S"]: #improve this with parseDirection proc
+        parsedCoordinate *= -1.0
     except IOError:
-        ## handle errors for not being floats either
+        ## handle errors
+  elif b.len == 1:
+    try:
+      parsedCoordinate = parseFloat(b[0])
+    except IOError:
+        ## handle errors for not being float either
   else:
     raise newException(IOError, "Invalid latitude or longitude.")
-    #parsefloat
-proc newPoint*(latitude, longitude: string): Point =
-  var 
-    lat = parseCoordinates(latitude)
-    long = parseCoordinates(longitude)
-  newPoint(lat, long)
+  parsedCoordinate
 ]#
 
